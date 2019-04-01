3dea55e76f7e2098a51569d5e3788579ffe5692f
louishenning81 <lhhenning@gmail.com>
2019-04-01 09:50:37 -0700

    correct file paths are up and running,need to add friends.js, apiRouts.js and connect to Heroku

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+
 var friends = [];
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "app", "public", "home.html"));
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "app", "public", "home.html"));
+// });
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "app", "public", "survey.html"));
-    console.log(path.join(__dirname, "app", "public", "survey.html"));
-});
+// app.get("/survey", function (req, res) {
+//     res.sendFile(path.join(__dirname, "app", "public", "survey.html"));
+//     console.log(path.join(__dirname, "app", "public", "survey.html"));
+// });  
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
