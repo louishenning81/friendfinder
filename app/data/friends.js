e091bf1f5060fad106c98d5dd487f536ceaa9f08
louishenning81 <lhhenning@gmail.com>
2019-04-02 17:40:54 -0700

    fixed algo, data correctlys sending to browser.  need form validation, new modal and actual questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -34,18 +34,18 @@ var users = [{
   Name: "yoda",
   link: "https://lumiere-a.akamaihd.net/v1/images/Yoda-Retina_2a7ecc26.jpeg?region=0%2C0%2C1536%2C864&width=960",
   score: [
-    1,
     2,
-    3,
-    4,
-    5,
-    1,
     2,
-    3,
-    4,
-    5
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2
   ]
 }
 ];
 
-module.exports = {users: users}
+module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,27 +1,68 @@
+var users = require("../data/friends.js");
+
+
 module.exports = function (app) {
 
-    var users = require("../data/friends.js");
-    
-    app.get("/api/friends", function(req, res){
-        return res.json(users);
+
+
+    app.get("/api/friends", function (req, res) {
+        res.json(users);
     });
-    
+
+
+
 
     app.post("/api/friends", function (req, res) {
         var newUser = req.body;
 
-        console.log(newUser);
-        
-        console.log(users);
-        
-        // app.get("/api/friends", function (req, res) {
-        //     var users = res.body;
-        //     console.log(users);
-        // })
-        // tables.push(users);
+        console.log(newUser.scores);
 
-        // res.json(newTable);
+        // console.log(users[0].score);
+
+
+        var resultarr = [];
+
+        function nextarray(arr) {
+            console.log("length = " + users.length);
+            for (i = 0; i < users.length; i++) {
+                console.log("Round : " + i);
+                var user = arr[i].score;
+                comp(user, newUser.scores)
+            };
+        }
+
+        function comp(arr1, arr2) {
+            var total = 0;
+            for (j = 0; j < arr2.length; j++) {
+                var compNum = Math.abs(arr1[j] - parseInt(arr2[j]));
+                total += compNum;
+                console.log(total);
+            }
+            resultarr.push(total);
+            total = 0;
+        };
+
+
+
+        nextarray(users);
+        console.log(resultarr);
+        users.push(newUser);
+        console.log(users.length);
+        console.log(users[resultarr.indexOf(Math.min(...resultarr))]);
+        res.json(users[resultarr.indexOf(Math.min(...resultarr))]);
+
+
+
+
+
+
     });
+
+    
+
 }
+
+
+
 
 
